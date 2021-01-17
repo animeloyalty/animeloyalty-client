c01212d3037736361864e94d8452f41096adea89
Deathspike <>
2021-01-17 19:39:18 +0100

    Fix base theme colors. WIP

--- src/shared/common/theme.ts
+++ src/shared/common/theme.ts
@@ -1,5 +1,6 @@
 import * as mui from '@material-ui/core';
 
 export const theme = mui.createMuiTheme({
-  breakpoints: {values: {xs: 0, sm: 0, md: 0, lg: 0, xl: 0}}
+  breakpoints: {values: {xs: 0, sm: 0, md: 0, lg: 0, xl: 0}},
+  palette: {primary: {main: '#333333'}, type: 'dark'}
 });
